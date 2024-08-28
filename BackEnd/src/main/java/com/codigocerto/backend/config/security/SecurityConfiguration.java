4d755feeae9e2d4f706ffabf6498e62630b564ed
Jânio <janioofi@gmail.com>
2024-08-27 22:39:14 -0300

    feat: Implementando a configuração do CORS na aplicação.

--- BackEnd/src/main/java/com/codigocerto/backend/config/security/SecurityConfiguration.java
+++ BackEnd/src/main/java/com/codigocerto/backend/config/security/SecurityConfiguration.java
@@ -1,4 +1,46 @@
 package com.codigocerto.backend.config.security;
 
-public class SecurityConfiguration {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration implements WebMvcConfigurer {
+
+    private static final String[] PUBLIC_URLS = {"http://localhost:4200"};
+    private static final String[] PUBLIC_METHODS = {"GET", "POST", "PUT", "DELETE", "OPTIONS"};
+    private static final String[] PUBLIC_HEADERS = {"Content-Type", "Authorization", "Access-Control-Allow-Origin"};
+    private static final String ALL = "/**";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(ALL).permitAll())
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of(PUBLIC_URLS));
+        corsConfiguration.setAllowedMethods(List.of(PUBLIC_METHODS));
+        corsConfiguration.setAllowedHeaders(List.of(PUBLIC_HEADERS));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(ALL, corsConfiguration);
+        return source;
+    }
 }
